d2d559f8a3488041648a1dc20626aa82bdcc85f9
jcherry <joshcherry@quickenloans.com>
2016-05-02 16:57:18 -0400

    End result

--- PublishGitHubRelease/PublishGitHubRelease.cs
+++ PublishGitHubRelease/PublishGitHubRelease.cs
@@ -6,83 +6,102 @@ using Octokit;
 
 namespace PublishGitHubRelease
 {
-	[Cmdlet("Publish", "GitHubRelease")]
+    [Cmdlet("Publish", "GitHubRelease")]
     public class PublishGitHubReleaseCmdlet : PSCmdlet
     {
 
-		[Parameter(Mandatory = true, Position = 1)]
-		public string ApplicationName { get; set; }
+        [Parameter(Mandatory = true, Position = 1)]
+        public string ApplicationName { get; set; }
 
-		[Parameter(Mandatory = true)]
-		public string Token { get; set; }
+        [Parameter(Mandatory = true)]
+        public string GitSourceOption { get; set; }
 
-		[Parameter(Mandatory = true)]
-		public string Repo { get; set; }
+        [Parameter(Mandatory = true)]
+        public string GitSourceUrl { get; set; }
 
-		[Parameter(Mandatory = true)]
-		public string Owner { get; set; }
+        [Parameter(Mandatory = true)]
+        public string Token { get; set; }
 
-		[Parameter(Mandatory = true)]
-		public string TagName { get; set; }
+        [Parameter(Mandatory = true)]
+        public string Repo { get; set; }
 
-		[Parameter(Mandatory = true)]
-		public string ReleaseName { get; set; }
+        [Parameter(Mandatory = true)]
+        public string Owner { get; set; }
 
-		[Parameter(Mandatory = false)]
-		public string ReleaseBody { get; set; }
+        [Parameter(Mandatory = true)]
+        public string TagName { get; set; }
 
-		[Parameter(Mandatory = true)]
-		public bool Draft { get; set; }
+        [Parameter(Mandatory = true)]
+        public string ReleaseName { get; set; }
 
-		[Parameter(Mandatory = false)]
-		public bool PreRelease { get; set; }
+        [Parameter(Mandatory = false)]
+        public string ReleaseBody { get; set; }
 
-		[Parameter(Mandatory = true)]
-		public string[] Assets { get; set; }
+        [Parameter(Mandatory = true)]
+        public bool Draft { get; set; }
 
-		protected override void ProcessRecord()
-	    {
-		    try
-		    {
-			    var client = new GitHubClient(new ProductHeaderValue(ApplicationName));
-			    var tokenAuth = new Credentials(Token); 
-			    client.Credentials = tokenAuth;
+        [Parameter(Mandatory = false)]
+        public bool PreRelease { get; set; }
 
-			    var newRelease = new NewRelease(TagName)
-			    {
-				    Name = ReleaseName,
-				    Body = ReleaseBody,					
-				    Draft = Draft,
-				    Prerelease = PreRelease,
-			    };
+        [Parameter(Mandatory = true)]
+        public bool AssetsOption { get; set; }
 
-			    var release = client.Repository.Release.Create(Owner, Repo, newRelease).Result;
-			    WriteVerbose("Created release with id: " + release.Id + " at " + release.Url);
-				WriteVerbose("Uploading " + Assets.Count() + " assets");
-			    foreach (var asset in Assets)
-			    {
-					WriteVerbose("Uploading " + asset);
-				    using (var archiveContents = File.OpenRead(asset))
-				    {
-					    var fileName = Path.GetFileName(asset);
-					    var assetUpload = new ReleaseAssetUpload()
-					    {
-						    FileName = fileName,
-						    ContentType = "application/zip",
-						    RawData = archiveContents
-					    };
+        [Parameter(Mandatory = true)]
+        public string[] Assets { get; set; }
 
-					    var uploadedAsset = client.Repository.Release.UploadAsset(release, assetUpload).Result;
-						WriteVerbose("Uploaded " + uploadedAsset.Name);
-				    }
-			    }
-		    }
-		    catch (Exception e)
-		    {
-				WriteVerbose("Error when publishing release: " + e);
-			    throw;
-		    }
-	    }
+        protected override void ProcessRecord()
+        {
+            try
+            {
+                var gitUri = GitHubClient.GitHubApiUrl;
+                if (GitSourceOption == "external")
+                {
+                    gitUri = new Uri(GitSourceUrl);
+                }
+
+                var client = new GitHubClient(new ProductHeaderValue(ApplicationName), gitUri);
+                var tokenAuth = new Credentials(Token);
+                client.Credentials = tokenAuth;
+
+                var newRelease = new NewRelease(TagName)
+                {
+                    Name = ReleaseName,
+                    Body = ReleaseBody,
+                    Draft = Draft,
+                    Prerelease = PreRelease,
+                };
+
+                var release = client.Repository.Release.Create(Owner, Repo, newRelease).Result;
+                WriteVerbose("Created release with id: " + release.Id + " at " + release.Url);
+
+                if (AssetsOption)
+                {
+                    WriteVerbose("Uploading " + Assets.Count() + " assets");
+                    foreach (var asset in Assets)
+                    {
+                        WriteVerbose("Uploading " + asset);
+                        using (var archiveContents = File.OpenRead(asset))
+                        {
+                            var fileName = Path.GetFileName(asset);
+                            var assetUpload = new ReleaseAssetUpload()
+                            {
+                                FileName = fileName,
+                                ContentType = "application/zip",
+                                RawData = archiveContents
+                            };
+
+                            var uploadedAsset = client.Repository.Release.UploadAsset(release, assetUpload).Result;
+                            WriteVerbose("Uploaded " + uploadedAsset.Name);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                WriteVerbose("Error when publishing release: " + e);
+                throw;
+            }
+        }
 
     }
 }
--- PublishGitHubRelease/PublishGitHubRelease.ps1
+++ PublishGitHubRelease/PublishGitHubRelease.ps1
@@ -1,6 +1,8 @@
 ﻿[cmdletbinding()]
 Param(
 	[string]$applicationName,
+	[string]$gitSourceOption,
+	[string]$gitSourceUrl,
 	[string]$token,
 	[string]$repo,
 	[string]$owner,
@@ -9,10 +11,13 @@ Param(
 	[string]$releaseBody,
 	[string]$draft,
 	[string]$prerelease,
+	[string]$assetsOption,
 	[string]$assetsPattern
 )
 Write-Verbose -Verbose "Entering script PublishRelease.ps1"
 Write-Verbose -Verbose "applicationName = $applicationName"
+Write-Verbose -Verbose "gitSourceOption = $gitSourceOption"
+Write-Verbose -Verbose "gitSourceUrl = $gitSourceUrl"
 Write-Verbose -Verbose "token = $token"
 Write-Verbose -Verbose "repo = $repo"
 Write-Verbose -Verbose "owner = $owner"
@@ -21,11 +26,14 @@ Write-Verbose -Verbose "releaseName = $releaseName"
 Write-Verbose -Verbose "releaseBody = $releaseBody"
 Write-Verbose -Verbose "draft = $draft"
 Write-Verbose -Verbose "prerelease = $prerelease"
+Write-Verbose -Verbose "assetsOption = $assetsOption"
 Write-Verbose -Verbose "assetsPattern = $assetsPattern"
+Write-Verbose -Verbose "version = 0.9.27"
 
 # Convert checkbox params to booleans
 [bool]$draftBool= Convert-String $draft Boolean
 [bool]$prereleaseBool= Convert-String $prerelease Boolean
+[bool]$assetsOptionBool= Convert-String $assetsOption Boolean
 
 # Import the Task.Common and Task.Internal dll that has all the cmdlets we need for Build
 import-module "Microsoft.TeamFoundation.DistributedTask.Task.Internal"
@@ -36,6 +44,10 @@ $scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 $pathToModule = Join-Path $scriptDir "PublishGitHubRelease.dll"
 import-module $pathToModule
 
-# Travers all matching files
-$assets = Find-Files -SearchPattern $assetsPattern
-Publish-GitHubRelease -ApplicationName $applicationName -Token $token -Repo $repo -Owner $owner -TagName $tagName -ReleaseName $releaseName -ReleaseBody $releaseBody -Draft $draftBool -PreRelease $prereleaseBool -Assets $assets
+[string[]]$assets = @()
+If ($assetsOptionBool -eq $true){
+	# Travers all matching files
+	$assets = Find-Files -SearchPattern $assetsPattern
+}
+
+Publish-GitHubRelease -ApplicationName $applicationName -GitSourceOption $gitSourceOption -GitSourceUrl $gitSourceUrl -Token $token -Repo $repo -Owner $owner -TagName $tagName -ReleaseName $releaseName -ReleaseBody $releaseBody -Draft $draftBool -PreRelease $prereleaseBool -AssetsOption $assetsOptionBool -Assets $assets
